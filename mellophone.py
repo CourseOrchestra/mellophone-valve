25777fb422f780c03a873a220dd2778fa0c9dfd8
Maria Prudyvus <maria.prudyvus@gmail.com>
2024-08-20 18:10:59 +0300

    fix import

--- mellophone.py
+++ mellophone.py
@@ -8,8 +8,6 @@ from uuid import uuid4
 import requests
 import xmltodict
 
-from src.utils.case_conversion import camel_to_snake_case
-
 
 class ForbiddenError(Exception):
     pass
@@ -27,11 +25,11 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-def xml2json(xml, key='user') -> dict[str, str]:
-    res = xmltodict.parse(xml)['user']
+def xml2json(xml, key="user") -> dict[str, str]:
+    res = xmltodict.parse(xml)["user"]
 
     for key in list(res.keys())[:]:
-        res[key.lstrip('@').lower()] = res.pop(key)
+        res[key.lstrip("@").lower()] = res.pop(key)
 
     return res
 
@@ -41,18 +39,19 @@ class Mellophone:
     """Класс для работы с mellophone
     Подробное описание сервлетов указано в https://courseorchestra.github.io/mellophone2/
     """
+
     _base_url: str
     set_settings_token: str
     user_manage_token: str
     session_id: str = None
 
     def __send_request(self, url, method="get", data=None):
-        url = f'{self._base_url}{url}'
-        if method == 'post':
+        url = f"{self._base_url}{url}"
+        if method == "post":
             res = {}
             for key in data.keys():
-                res[f'@{key}'] = data[key]
-            data = xmltodict.unparse({'user': res})
+                res[f"@{key}"] = data[key]
+            data = xmltodict.unparse({"user": res})
             response = requests.post(url, data=data)
         else:
             response = requests.get(url)
@@ -77,13 +76,13 @@ class Mellophone:
             ip {str} -- ip компьютера пользователя для передачи в ф-цию проверки пользователя по логину и ip (default: {None})
         """
         ses_id = ses_id or self.session_id or str(uuid4())
-        url = f'/login?sesid={ses_id}&login={login}&pwd={password}'
+        url = f"/login?sesid={ses_id}&login={login}&pwd={password}"
 
         if gp is not None:
-            url += '&gp={}'.format(gp)
+            url += "&gp={}".format(gp)
 
         if ip:
-            url += '&ip={}'.format(ip)
+            url += "&ip={}".format(ip)
 
         self.__send_request(url)
 
@@ -94,19 +93,19 @@ class Mellophone:
         Keyword Arguments:
             ses_id {str} -- id сессии (по умолчанию текущая) (default: {None})
         """
-        url = '/logout?&sesid={}'.format(ses_id or self.session_id)
+        url = "/logout?&sesid={}".format(ses_id or self.session_id)
 
         self.__send_request(url)
 
     def create_user(self, user):
-        url = f'/user/create?token={self.user_manage_token}'
-        if 'password' in user:
-            user['pwd'] = user.pop('password')
+        url = f"/user/create?token={self.user_manage_token}"
+        if "password" in user:
+            user["pwd"] = user.pop("password")
         return self.__send_request(url, method="post", data=user)
 
     def update_user(self, sid, token, user):
 
-        url = f'/user/{sid}?token={token}'
+        url = f"/user/{sid}?token={token}"
         user = json.dumps(user)
         return self.__send_request(url, method="post", data=user)
 
@@ -117,7 +116,7 @@ class Mellophone:
         Returns:
             json -- информация о пользователе
         """
-        url = f'/isauthenticated?sesid={ses_id or self.session_id}'
+        url = f"/isauthenticated?sesid={ses_id or self.session_id}"
         try:
             response = self.__send_request(url)
         except ForbiddenError:
@@ -133,7 +132,7 @@ class Mellophone:
         Keyword Arguments:
             old_ses_id {str} -- старая сессия (по умолчанию текущая) (default: {None})
         """
-        url = f'/changeappsesid?oldsesid={ses_id or self.session_id}&newsesid={new_ses_id}'
+        url = f"/changeappsesid?oldsesid={ses_id or self.session_id}&newsesid={new_ses_id}"
         self.__send_request(url)
 
     def check_name(self, name, ses_id=None):
@@ -146,7 +145,7 @@ class Mellophone:
         Returns:
             json -- информация о пользователе
         """
-        url = f'/checkname?sesid={ses_id or self.session_id}&name={name}'
+        url = f"/checkname?sesid={ses_id or self.session_id}&name={name}"
         response = self.__send_request(url)
         return xml2json(response)
 
@@ -155,7 +154,7 @@ class Mellophone:
         Returns:
             list -- список групп провайдеров
         """
-        url = '/importgroupsproviders'
+        url = "/importgroupsproviders"
         response = self.__send_request(url)
         return response.split()
 
@@ -170,7 +169,7 @@ class Mellophone:
         Returns:
             [type] -- [description]
         """
-        url = f'/changepwd?sesid={ses_id or self.session_id}&oldpwd={old_pwd}&newpwd={new_pwd}'
+        url = f"/changepwd?sesid={ses_id or self.session_id}&oldpwd={old_pwd}&newpwd={new_pwd}"
         self.__send_request(url)
 
     def change_user_pwd(self, username, old_pwd, new_pwd, ses_id=None):
@@ -184,7 +183,7 @@ class Mellophone:
         Returns:
             [type] -- [description]
         """
-        url = f'/changeuserpwd?sesid={ses_id or self.session_id}&oldpwd={old_pwd}&newpwd={new_pwd}&username={username}'
+        url = f"/changeuserpwd?sesid={ses_id or self.session_id}&oldpwd={old_pwd}&newpwd={new_pwd}&username={username}"
         self.__send_request(url)
 
     def check_credentials(self, login, password, gp=None, ip=None):
@@ -198,13 +197,13 @@ class Mellophone:
         Returns:
             json -- Информация о пользователе
         """
-        url = '/checkcredentials?login={}&pwd={}'.format(login, password)
+        url = "/checkcredentials?login={}&pwd={}".format(login, password)
 
         if gp is not None:
-            url += '&gp={}'.format(gp)
+            url += "&gp={}".format(gp)
 
         if ip:
-            url += '&ip={}'.format(ip)
+            url += "&ip={}".format(ip)
 
         response = self.__send_request(url)
         return xml2json(response)
@@ -220,15 +219,15 @@ class Mellophone:
         Returns:
             list -- Список провайдеров
         """
-        url = '/getproviderlist?login={}&pwd={}'.format(login, password)
+        url = "/getproviderlist?login={}&pwd={}".format(login, password)
 
         if gp is not None:
-            url += '&gp={}'.format(gp)
+            url += "&gp={}".format(gp)
 
         if ip:
-            url += '&ip={}'.format(ip)
+            url += "&ip={}".format(ip)
 
-        return xmltodict.parse(self.__send_request(url))['providers']
+        return xmltodict.parse(self.__send_request(url))["providers"]
 
     def get_user_list(self, token, gp, ip=None, pid=None):
         """Возвращает информацию о пользователях провайдера
@@ -241,13 +240,13 @@ class Mellophone:
         Returns:
             list -- Список пользователей провайдера
         """
-        url = '/getuserlist?token={}&gp={}'.format(token, gp)
+        url = "/getuserlist?token={}&gp={}".format(token, gp)
 
         if ip:
-            url += '&ip={}'.format(ip)
+            url += "&ip={}".format(ip)
 
         if pid:
-            url += '&ip={}'.format(ip)
+            url += "&ip={}".format(ip)
 
         response = self.__send_request(url)
 
@@ -263,10 +262,10 @@ class Mellophone:
         Returns:
             [type] -- [description]
         """
-        url = '/setsettings?token={}'.format(token)
+        url = "/setsettings?token={}".format(token)
         if lockout_time:
-            url += '&lockouttime={}'.format(lockout_time)
+            url += "&lockouttime={}".format(lockout_time)
 
         if login_attempts_allowed:
-            url += '&loginattemptsallowed={}'.format(login_attempts_allowed)
+            url += "&loginattemptsallowed={}".format(login_attempts_allowed)
         self.__send_request(url)
